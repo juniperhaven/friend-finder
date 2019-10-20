888ef8dfcd545318b95666c7ce6afe8a1b0b3af7
Cedar <alexianfireflies@gmail.com>
2019-10-19 21:33:54 -0700

    added some api routes and some server functionality

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [
+    {name: "Lapis Lazuli",
+    photo: "https://66.media.tumblr.com/d2ccc5dae679ed0244d26c3f13a53e46/tumblr_pb7gtrTYQ41v5pucyo1_250.png",
+    scores: [1, 2, 3, 2, 5, 4, 4, 4, 2, 1]},
+
+    {name: "Amethyst",
+    photo: "https://66.media.tumblr.com/31de10f58e704596bd138eebb1629e0e/tumblr_pxbewbP13O1rtctzxo9_250.png",
+    scores: [4, 3, 2, 1, 5, 2, 5, 1, 3, 5]},
+
+    {name: "Pearl",
+    photo: "https://66.media.tumblr.com/ebafe0d29cfc725e6e2a49e49aad9a3f/tumblr_p8e4ig86Um1rtctzxo7_250.png",
+    scores: [1, 4, 2, 5, 2, 2, 3, 2, 2, 2]},
+
+    {name: "Garnet",
+    photo: "https://pm1.narvii.com/6921/a3329cb8480b4589ba685efe918b0870f2c4a881r1-250-250v2_hq.jpg",
+    scores: [2, 2, 5, 4, 1, 5, 1, 5, 2, 1]},
+
+    {name: "Steven Universe",
+    photo: "https://66.media.tumblr.com/7f1d7af8b1e74c5ae561a3e25a5dae6c/tumblr_puwtmbWmSg1rtctzxo8_250.png",
+    scores: [5, 5, 4, 2, 4, 4, 5, 2, 5, 4]},
+
+    {name: "Connie Maheswaran",
+    photo: "https://66.media.tumblr.com/64ad72489374d1588cbf8334ff16e66a/tumblr_plqy0eRuB01xovcw2o4_250.png",
+    scores: [1, 2, 2, 5, 2, 4, 2, 3, 3, 2]},
+
+    {name: "Peridot",
+    photo: "https://66.media.tumblr.com/5d55173beecfbc6d64b29b7112591554/tumblr_p848a2x6Uc1v5y27go4_250.jpg",
+    scores: [1, 2, 2, 5, 1, 1, 1, 4, 1, 2]}
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // requires express and path
-var express = require("express");
+const express = require("express");
 var path = require("path");
 
 // set up for express
-var app = express();
+const app = express();
 var PORT = process.env.PORT || 3000;
 
 // set up for express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
