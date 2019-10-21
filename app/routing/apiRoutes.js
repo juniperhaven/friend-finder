9a40be920e3cfe0c75d930dc195af54d6619d960
Cedar <alexianfireflies@gmail.com>
2019-10-21 13:30:28 -0700

    added html routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,43 +1,41 @@
 var path = require("path");
 var friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
-        var html = "<h1>Possible Friends</h1>"
+app.get("/api/friends", function(req, res) {
+    var html = "<h1>Possible Friends</h1>"
 
-        for(var i = 0; i < friends.length; i++) {
-            html += "<p>Name: "+friends[i].name+"</p>"
-            html += "<img src = "+friends[i].photo+">"
+    for(var i = 0; i < friends.length; i++) {
+        html += "<p>Name: "+friends[i].name+"</p>"
+        html += "<img src = "+friends[i].photo+">"
+    }
+
+    res.send(html);
+});
+
+app.post("/api/friends", function(req, res) {
+    var user = req.body;
+    var userScores = user.scores;
+
+    var friendName;
+    var friendPic;
+    var totalDiff = 50; // according to my calculations, if all I'm doing is taking all the potential-friend scores that aren't the same as the user scores, given that 5 is the highest value, 1 is the lowest value, and there's 10 questions, the highest possible difference is 40, but I'm not sure of my math, so I set it at 50, because honestly that should work fine.
+
+    for(var i = 0; i < friends.length; i++) {
+        var diff = 0;
+
+        for(var j = 0; j < userScores.length; j++) {
+            diff += Math.abs(friends[i].scores[j]-userScores[j]);
         }
 
-        res.send(html);
-    });
+        console.log(diff);
 
-    app.post("/api/friends", function(req, res) {
-        var user = req.body;
-        var userScores = user.scores;
-
-        var friendName;
-        var friendPic;
-        var totalDiff = 50; // according to my calculations, if all I'm doing is taking all the potential-friend scores that aren't the same as the user scores, given that 5 is the highest value, 1 is the lowest value, and there's 10 questions, the highest possible difference is 40, but I'm not sure of my math, so I set it at 50, because honestly that should work fine.
-
-        for(var i = 0; i < friends.length; i++) {
-            var diff = 0;
-
-            for(var j = 0; j < userScores.length; j++) {
-                diff += Math.abs(friends[i].scores[j]-userScores[j]);
-            }
-
-            console.log(diff);
-
-            if(diff < totalDiff) {
-                friendName = friends[i].name;
-                friendPic = friends[i].photo;
-            }
+        if(diff < totalDiff) {
+            friendName = friends[i].name;
+            friendPic = friends[i].photo;
         }
+    }
 
-        friends.push(user);
+    friends.push(user);
 
-        res.send({friendName, friendPic});
-    });
-}
+    res.send({friendName, friendPic});
+});
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // requires express and path
-const express = require("express");
+var express = require("express");
 var path = require("path");
 
 // set up for express
-const app = express();
+var app = express();
 var PORT = process.env.PORT || 3000;
 
 // set up for express to handle data parsing
@@ -12,14 +12,6 @@ app.use(express.json());
 
 var apiRoutes = require("./app/routing/apiRoutes");
 var htmlRoutes = require("./app/routing/htmlRoutes");
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
